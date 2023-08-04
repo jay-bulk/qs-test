72dd9369afd391236373903397e8c063979544e9
jay-bulk <rbulkley@byu.edu>
2023-08-04 00:57:48 -0600

    WIP: qs on app.ts

--- app.ts
+++ app.ts
@@ -1,8 +1,15 @@
 import Fastify, {FastifyInstance} from 'fastify'
 import db from "./plugins/db";
 import auth from './plugins/auth'
+import qs from 'qs'
+
 export async function app (): Promise<FastifyInstance> {
-  let fastify: FastifyInstance = Fastify({})
+  
+  let fastify: FastifyInstance = Fastify({
+    querystringParser: (query) => {
+      qs.parse(str, {comma: true})
+    }
+  })
   await fastify.register(db)
   await fastify.register(auth)
 
